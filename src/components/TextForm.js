691b7a5b21f66cbafa547bdf70af248a1ba8ac47
Wahid Ali <aliwahid1716@gmail.com>
2022-07-02 14:36:54 +0530

    added uppercase and lowercase function with word count

---
+++ src/components/TextForm.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+
+    const handleUpClick = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+
+    const handleLoClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+    }
+
+    const [text, setText] = useState('');
+
+    return (
+        <>
+            <div className='container'>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+            </div>
+
+            <div className="container my-3">
+                <h2>Your text summary</h2>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").length} Minutes read</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
+
+        </>
+    )
+}
